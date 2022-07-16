26530d3901453fa1417677a9c756f387bb6d2a33
Thanh Dat <datkull456@gmail.com>
2022-07-16 09:29:42 +0700

    first commit

---
+++ CSDL.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dbo].[chucvu](
+	[ma] [varchar](10) NOT NULL,
+	[tenchucvu] [nvarchar](15) NULL
+)
+GO
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'01', N'Giám đốc')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'02', N'Phó giám đốc')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'03 ', N'Thư ký')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'04 ', N'Nhân viên')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'05', N'Bảo vệ')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'06', N'Thu ngân')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'07', N'Thực tập sinh')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'08', N'Trưởng Phòng')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'09', N'Phó Phòng')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'10', N'Tổ Phó')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'11', N'Tổ Trưởng')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'12', N'Công Nhân')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'13', N'Thư Ký')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'14', N'Trợ Lý')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'15', N'Bộ phận tuyển dụng')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'16', N'Bộ phận phụ trách đào tạo')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'17', N'Bộ phận phụ trách bảo hiểm')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'18', N'Chuyên viên tuyển dụng')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'19', N'Quản trị hành chính')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'20', N'Chuyên viên đào tạo')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'21', N'Chuyên viên')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'22', N'Thực tập sinh')
+INSERT [dbo].[chucvu] ([ma], [tenchucvu]) VALUES (N'23', N'Thực tập sinh')
+
+
